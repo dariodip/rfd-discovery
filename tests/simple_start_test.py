59ef0dc97e63b197898f0c047d2fd4f48265c447
Mattia Tomeo <tomeomattia@yahoo.it>
2016-12-20 13:52:59 +0100

    aggiunto oracolo
    Ora il metodo test_distance_mtr_control_example utilizza un oracolo, generato con il metodo "dist" del linguaggio R, per verificare che i valori calcolati siano esatti.

--- tests/simple_start_test.py
+++ tests/simple_start_test.py
@@ -1,7 +1,12 @@
 import unittest
 import os.path as op
+import pandas as pnd
+import numpy as np
+
+from math import sqrt
 from random import randint
 from loader import distance_mtr
+from ast import literal_eval
 
 DATASET_CSV = "../resources/dataset.csv"
 DS_CSV = "../resources/distance-ds_test.csv"
@@ -25,6 +30,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual((split_rhs.shape[1] + split_lhs.shape[1]), 4, "check if the sum of RHSs and LHSs is 4")
         dist_m = dm.distance_matrix(split)  # create distance matrix according to split on RHS and LHS
         keys = dist_m.index
+        print(keys)
         expected_pairs = int(len(split_rhs) * (len(split_rhs) - 1) / 2)  # (n*n-1)/2 pairs on n els
         self.assertEqual(len(keys), expected_pairs, "check if number of pairs is n*(n-1)/2")
         # generate a random pair of indexes
@@ -38,6 +44,20 @@ class MyTestCase(unittest.TestCase):
         h_distance_row = dist_m['RHS']
         lhdr = h_distance_row.tolist()
         self.assertTrue(all(lhdr[i] >= lhdr[i+1] for i in range(len(lhdr)-1)), "check if distance on RHS is sorted")
+
+        #check if the distances correspond to the value in the oracle
+        oracle = pnd.read_csv("../resources/dataset_oracle.csv", sep=",", header=0, index_col=0)
+
+        for key in keys:
+            key_tuple = literal_eval(key) #extract the value from the tuple in a string form
+            row, col = key_tuple
+            a_tuple = dist_m.loc[key].tolist()
+            # complete the formula of the euclidean's distances of the R's method dist
+            distance = sqrt(sum(x**2 for x in a_tuple))
+            #oracle.loc[row index] return a Series with the index in string form
+            #never check the equality of two floating numbers with the equals' operator
+            self.assertTrue(np.isclose(distance, oracle.loc[row][str(col)]), "value error")
+
         dist_m.to_csv(DS_CSV, sep=";")  # save distance csv in resources
         self.assertTrue(op.isfile(DS_CSV), "check if distance matrix is saved")
         self.assertTrue(op.exists(DS_CSV), "check if distance matrix is saved")
