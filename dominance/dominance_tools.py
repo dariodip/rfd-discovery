a3b7010c479c48e3e746386101194ca883374e49
dario <darionovantadue@gmail.com>
2016-12-21 10:01:16 +0100

    trasformato modulo in classe

--- dominance/dominance_tools.py
+++ dominance/dominance_tools.py
@@ -3,61 +3,60 @@ import pandas as pnd
 import numpy as np
 
 
-def get_dominance(path: str, dominance_funct):
-    diff_mtx = DiffMatrix(path)
-    diff_mtx.load()
-    distance_matrix = diff_mtx.distance_matrix(diff_mtx.split_sides([1,2,3], [0]))
-    print(distance_matrix)
-    dominance_funct(distance_matrix, [1, 2, 3], [0])
+class NaiveDominance:
 
+    def __init__(self):
+        self.tuples_set = set()
+        self.distance_matrix = None
+        self.on_distance_dom = dict()
 
-def naive_dominance(d_mtx: pnd.DataFrame, lhs: list, rhs: list):
-    list_of_dominance_set = list()
-    distance_values = list(set(np.asarray(d_mtx.iloc[:, rhs].values, dtype='int').flatten()))
-    distance_values.sort(reverse=True)
-    previous = set()
-    on_distance_dom = dict()
-    max_dist = max(distance_values)
-    for dist in distance_values:
-        df_act_dist = d_mtx[d_mtx.RHS == dist]
-        for index, row in df_act_dist[lhs].iterrows():
-            last_row = tuple(row.values.tolist())
-            if len(previous) == 0 or check_dominance(last_row, previous):
-                previous.add(last_row)
-                add_to_dict_set(on_distance_dom, last_row, dist)
-    print(previous)
-    #print(on_distance_dom)
-    #print(len(previous))
+    def get_dominance(self, path: str, dominance_funct, HSs : dict):
+        diff_mtx = DiffMatrix(path)
+        diff_mtx.load()
+        self.distance_matrix = diff_mtx.distance_matrix(diff_mtx.split_sides(HSs['lhs'], HSs['rhs']))
+        dominance_funct(self.distance_matrix, HSs['lhs'], HSs['rhs'])
 
+    def naive_dominance(self, d_mtx: pnd.DataFrame, lhs: list, rhs: list):
+        distance_values = list(set(np.asarray(d_mtx.iloc[:, rhs].values, dtype='int').flatten()))
+        distance_values.sort(reverse=True)
+        max_dist = max(distance_values)
+        for dist in distance_values:
+            df_act_dist = d_mtx[d_mtx.RHS == dist]
+            for index, row in df_act_dist[lhs].iterrows():
+                last_row = tuple(row.values.tolist())
+                if len(self.tuples_set) == 0 or self.check_dominance(last_row):
+                    self.tuples_set.add(last_row)
+                    self.__add_to_dict_set(last_row, dist)
+        print(self.tuples_set)
+        # print(on_distance_dom)
+        # print(len(previous))
 
-def check_dominance(y: tuple, tuples_set: set) -> bool:
-    # X dominates X iff foreach x in X, foreach y in Y, x < y <=> x - y < 0
-    to_remove = set()
-    if len(tuples_set) == 0:
-        return True
-    for x in tuples_set:
-        diff = np.array(x) - np.array(y)
-        if all(diff < 0):  # X dominates Y
-            if len(to_remove) == 0:
-                return False
-            else:
-                break
-        elif all(diff >= 0):  # Y dominates X
-            to_remove.add(x)
-    if len(to_remove) == 0:
-        return True
-    else:
-        #tuples_set = tuples_set - to_remove
-        for el in to_remove:
-            tuples_set.remove(el)
-        return True
+    def check_dominance(self, y: tuple) -> bool:
+        # X dominates X iff foreach x in X, foreach y in Y, x <= y <=> x - y <= 0
+        to_remove = set()
+        if len(self.tuples_set) == 0:
+            return True
+        for x in self.tuples_set:
+            diff = np.array(x) - np.array(y)
+            if all(diff <= 0):  # X dominates Y
+                if len(to_remove) == 0:
+                    return False
+                else:
+                    break
+            elif all(diff >= 0):  # Y dominates X
+                to_remove.add(x)
+        if len(to_remove) == 0:
+            return True
+        else:
+            self.tuples_set = self.tuples_set - to_remove
+            return True
 
-
-def add_to_dict_set(d: dict, to_add: tuple, i: int):
-    if i not in d:
-        d[i] = set()
-    d[i].add(to_add)
+    def __add_to_dict_set(self, to_add: tuple, i: int):
+        if i not in self.on_distance_dom:
+            self.on_distance_dom[i] = set()
+        self.on_distance_dom[i].add(to_add)
 
 
 if __name__ == "__main__":
-    get_dominance("../resources/dataset.csv", naive_dominance)
+    nd = NaiveDominance()
+    nd.get_dominance("../resources/dataset.csv", nd.naive_dominance, {'lhs': [1, 2, 3], 'rhs': [0]})
