4a891e08cd408f7b1565b369f1fdce2b96c9d987
darioskal <dipasquale.d@tiscali.it>
2016-12-22 17:16:33 +0100

    risolto problema della rimozione durante l'iterazione

--- dominance/dominance_tools.py
+++ dominance/dominance_tools.py
@@ -36,37 +36,29 @@ class NaiveDominance:
 
     def check_dominance(self, y: tuple, dist) -> bool:
         # X dominates X iff foreach x in X, foreach y in Y, x <= y <=> x - y <= 0
-        to_remove = set()
         if len(self.tuples_set) == 0:
             return True
-        for x in self.tuples_set:
+        for x in list(self.tuples_set):
             diff = np.array(x) - np.array(y)
             if all(diff <= 0):  # X dominates Y
-                if len(to_remove) == 0:
-                    return False
-                else:
-                    break
+                return False
             elif all(diff >= 0):  # Y dominates X
-                to_remove.add(x)
-        if len(to_remove) == 0:
-            return True
-        else:
-            self.tuples_set = self.tuples_set - to_remove
-            return True
+                self.tuples_set.remove(x)
+        return True
 
     def clean_tuple_set(self, rows_to_add: set) -> set:
         if len(rows_to_add) == 1:
             return rows_to_add
-        l_rows = list(rows_to_add)
-        for i in range(0, len(l_rows)):
-            for j in range(i+1, len(l_rows)):
-                diff = np.array(l_rows[i]) - np.array(l_rows[j])
+        list_rows = list(rows_to_add)
+        for i in range(0, len(list_rows)):
+            for j in range(i+1, len(list_rows)):
+                diff = np.array(list_rows[i]) - np.array(list_rows[j])
                 if all(diff >= 0):
-                    if l_rows[j] in rows_to_add:
-                        rows_to_add.remove(l_rows[j])
+                    if list_rows[j] in rows_to_add:
+                        rows_to_add.remove(list_rows[j])
                 elif all(diff <= 0):
-                    if l_rows[i] in rows_to_add:
-                        rows_to_add.remove(l_rows[i])
+                    if list_rows[i] in rows_to_add:
+                        rows_to_add.remove(list_rows[i])
         return rows_to_add
 
     def __add_to_dict_set(self, to_add: set, i: int):
@@ -76,5 +68,8 @@ class NaiveDominance:
 
 
 if __name__ == "__main__":
+    import time
     nd = NaiveDominance()
-    print(nd.get_dominance("../resources/dataset.csv", nd.naive_dominance, {'lhs': [1, 2, 3], 'rhs': [0]}))
+    start = time.clock()
+    print(nd.get_dominance("../resources/dataset.csv", nd.naive_dominance, {"lhs": [1, 2, 3], "rhs": [0]}))
+    print("elapsed time: ", time.clock() - start)
