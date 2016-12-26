7a8ce96e4ff66c39868d331e8eeb27adad7d9e44
mattiatomeo <tomeo.mat@gmail.com>
2016-12-26 16:48:36 +0100

    modificata la classe NaiveDominance.
    Ora la funzione naive_dominance restituisce il dataframe di partenza, in cui vengono rimosse le tuple dominate.
    self.tuples_set(diventato ora self.tuples_dict) e rows_to_add sono diventati dizionari in cui ogni tupla viene indicizzata con la corrispondente chiave del data frame.

--- dominance/dominance_tools.py
+++ dominance/dominance_tools.py
@@ -6,7 +6,7 @@ import numpy as np
 class NaiveDominance:
 
     def __init__(self):
-        self.tuples_set = set()
+        self.tuples_dict = dict()
         self.distance_matrix = None
         self.on_distance_dom = dict()
 
@@ -14,7 +14,8 @@ class NaiveDominance:
         diff_mtx = DiffMatrix(path)
         diff_mtx.load()
         self.distance_matrix = diff_mtx.distance_matrix(diff_mtx.split_sides(HSs['lhs'], HSs['rhs']))
-        return dominance_funct(self.distance_matrix, HSs['lhs'], HSs['rhs'])
+        self.distance_matrix = dominance_funct(self.distance_matrix, HSs['lhs'], HSs['rhs'])
+        return self.distance_matrix
 
     def naive_dominance(self, d_mtx: pnd.DataFrame, lhs: list, rhs: list) -> pnd.DataFrame:
         distance_values = list(set(np.asarray(d_mtx.iloc[:, rhs].values, dtype='int').flatten()))
@@ -24,41 +25,43 @@ class NaiveDominance:
         df_keys.remove('RHS')
         for dist in distance_values:
             df_act_dist = d_mtx[d_mtx.RHS == dist]
-            rows_to_add = set()
+            rows_to_add = dict()
             for index, row in df_act_dist[df_keys].iterrows():
                 last_row = tuple(row.values.tolist())
-                if len(self.tuples_set) == 0 or self.check_dominance(last_row, dist):
-                    rows_to_add.add(last_row)
-            rows_to_add = self.clean_tuple_set(rows_to_add)
-            self.tuples_set = self.tuples_set.union(rows_to_add)
+                if len(self.tuples_dict) == 0 or self.check_dominance(last_row, dist):
+                     rows_to_add[index] = last_row
+            rows_to_add = self.clean_tuple_dict(rows_to_add)
+            self.tuples_dict.update(rows_to_add)
             self.__add_to_dict_set(rows_to_add, dist)
-        return self.on_distance_dom
+        print(self.tuples_dict)
+        print(self.on_distance_dom)
+        return d_mtx[d_mtx.index.map(lambda x: x in self.tuples_dict.keys())]
 
     def check_dominance(self, y: tuple, dist) -> bool:
         # X dominates X iff foreach x in X, foreach y in Y, x <= y <=> x - y <= 0
-        if len(self.tuples_set) == 0:
+        if len(self.tuples_dict) == 0:
             return True
-        for x in list(self.tuples_set):
-            diff = np.array(x) - np.array(y)
+        for x in list(self.tuples_dict.keys()):
+            diff = np.array(self.tuples_dict[x]) - np.array(y)
             if all(diff <= 0):  # X dominates Y
                 return False
             elif all(diff >= 0):  # Y dominates X
-                self.tuples_set.remove(x)
+                del self.tuples_dict[x]
         return True
 
-    def clean_tuple_set(self, rows_to_add: set) -> set:
+    def clean_tuple_dict(self, rows_to_add: dict) -> dict:
         if len(rows_to_add) == 1:
             return rows_to_add
-        list_rows = list(rows_to_add)
-        for i in range(0, len(list_rows)):
-            for j in range(i+1, len(list_rows)):
-                diff = np.array(list_rows[i]) - np.array(list_rows[j])
+        row_index = list(rows_to_add.keys())
+        for i in range(0, len(row_index)):
+            for j in range(i+1, len(row_index)):
+                diff = np.array(rows_to_add[row_index[i]]) - np.array(rows_to_add[row_index[j]])
                 if all(diff >= 0):
-                    if list_rows[j] in rows_to_add:
-                        rows_to_add.remove(list_rows[j])
+                    if rows_to_add[row_index[j]] in rows_to_add:
+                        del rows_to_add[row_index[j]]
                 elif all(diff <= 0):
-                    if list_rows[i] in rows_to_add:
-                        rows_to_add.remove(list_rows[i])
+                    if rows_to_add[row_index[i]] in rows_to_add:
+                        del rows_to_add[row_index[i]]
         return rows_to_add
 
     def __add_to_dict_set(self, to_add: set, i: int):
