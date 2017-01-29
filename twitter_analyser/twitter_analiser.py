4a8306fc58fb7bd6e359330019ef54cfac9b9b57
darioskal <dipasquale.d@tiscali.it>
2017-01-29 19:45:11 +0100

    aggiunti logger al server

--- twitter_analyser/twitter_analiser.py
+++ twitter_analyser/twitter_analiser.py
@@ -5,9 +5,19 @@ import tornado.web
 from twitter_analyser import web_socket_listener
 from threading import Timer
 import sys
+import logging
 
-TEN_MINUTES = 10*60
+SERVER_PORT = 9091
 TWENTY_FOUR_HOURS = 24*60*60
+BACKUP_TIME = 60*60
+TEN_MINUTES = 10*60
+
+logging.basicConfig(level=logging.INFO)
+fh = logging.FileHandler("starter.log")
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger_ = logging.getLogger(__name__)
+logger_.addHandler(fh)
 
 def choose_tweets(on_method = True):
     Timer(TEN_MINUTES, choose_tweets).start()
@@ -21,10 +31,11 @@ application = tornado.web.Application([
 
 if __name__ == "__main__":
     http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(9091)
-    print("Server in ascolto")
+    http_server.listen(SERVER_PORT)
+    logger_.info("Server in ascolto sulla porta %d", SERVER_PORT)
     choose_tweets(False)
     server_instance = tornado.ioloop.IOLoop.instance()
+    Timer(BACKUP_TIME, web_socket_listener.backup).start()
     Timer(TWENTY_FOUR_HOURS, web_socket_listener.save_file_and_close).start()
     server_instance.start()
 
--- twitter_analyser/web_socket_listener.py
+++ twitter_analyser/web_socket_listener.py
@@ -9,6 +9,14 @@ import csv
 import pandas as pd
 import random as rnd
 import sys
+import logging
+
+logging.basicConfig(level=logging.INFO)
+fh = logging.FileHandler("server.log")
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger_ = logging.getLogger(__name__)
+logger_.addHandler(fh)
 
 pandas_cols = ['created_at', 'sentiment_word_count', 'sentiment_comparative', 'sentiment_score',
                  'sentiment_positive_count', 'sentiment_tokens', 'text', '_msgid', 'retweet_count',
@@ -16,21 +24,23 @@ pandas_cols = ['created_at', 'sentiment_word_count', 'sentiment_comparative', 's
 session_data_frame = pd.DataFrame(columns=pandas_cols)
 global_data_frame = pd.DataFrame(columns=pandas_cols)
 CHOSEN_ROWS = 5
+BACKUP_TIME = 60*60
+
 
 
 class WebSocketListener(tornado.websocket.WebSocketHandler):
 
-    def open(self, ):
-        print("Nuova connessione in entrata")
+    def open(self):
+        logger_.info("Connessione in entrata.")
 
     def on_message(self, message):
+        logger_.debug("Ricevuto messaggio")
         mgs_dict = json.loads(message)
         p_n_s = ParserAndSaver(mgs_dict)
         p_n_s.run()
 
     def on_close(self):
-
-        print("Connessione chiusa")
+        logger_.warning("Connessione chiusa")
 
     def check_origin(self, origin):
         return True
@@ -49,20 +59,28 @@ class ParserAndSaver(threading.Thread):
         for k in pandas_cols:
             pandas_row.append(self.message_d[k])
         session_data_frame.loc[session_data_frame.shape[0]] = pandas_row
+        logger_.debug("Messaggio aggiunto al data frame")
 
 def choose_rows():
+    logger_.info("Procedura di selezione delle righe iniziata.")
     df_len = session_data_frame.shape[0]
     sample = rnd.sample(range(0,df_len), CHOSEN_ROWS)
     for s in sample:
         global_data_frame.loc[global_data_frame.shape[0]] = session_data_frame.loc[s]
+    logger_.info("Procedura di selezione delle righe terminata")
 
 
 def save_file_and_close():
-    print("Saving files")
+    logger_.info("Salvataggio del file")
     global_data_frame.to_csv("crawled-tweets.csv", sep=";")
+    logger_.info("File salvato")
     quit()
 
 def backup():
-    threading.Timer(60*60, backup).start()
-    print("Saving backup")
-    global_data_frame.to_csv("crawled-tweets.csv.bak", sep=";")
+    threading.Timer(BACKUP_TIME, backup).start()
+    logger_.info("Avvio backup")
+    try:
+        global_data_frame.to_csv("crawled-tweets.csv.bak", sep=";")
+        logger_.info("Backup terminato con successo")
+    except Exception:
+        logger_.critical("Backup fallito")
