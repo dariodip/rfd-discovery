206a25d58be7ea4c4551b7704e33d6baa0d40827
dario <darionovantadue@gmail.com>
2017-02-01 15:01:19 +0100

    velocizzati tempi di accesso al dataframe delle distanze

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ hss = {"lhs": [1, 2, 3], "rhs": [0]}
 if __name__ == "__main__":
     with ut.timeit_context("Whole time"):
         with ut.timeit_context("Distance time"):
-            diff_mtx = DiffMatrix("resources/dataset.csv", {})
+            diff_mtx = DiffMatrix("resources/iris.csv", {}, first_col_header=None, sep=",")
             diff_mtx.load()
             dist_mtx = diff_mtx.distance_matrix(hss)
             diff_mtx = None     # for free unused memory
