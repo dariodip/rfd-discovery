2238f8d17d3b21975e37cc3d5fd368ab904df5b8
darioskal <dipasquale.d@tiscali.it>
2017-02-02 10:25:53 +0100

    aggiunto metodo per rilevare automaticamente header e separatore dei csv

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,6 @@
 from contextlib import contextmanager
 import time
+import csv
 
 
 @contextmanager
@@ -8,3 +9,11 @@ def timeit_context(name):
     yield
     elapsedTime = time.time() - startTime
     print('[{}] finished in {:.2f} ms'.format(name, elapsedTime * 1000))
+
+
+def check_sep_n_header(csv_file):
+    with open(csv_file) as csv_f:
+        sniffer = csv.Sniffer()
+        dialect = sniffer.sniff(csv_f.readline())
+        has_header = sniffer.has_header(csv_f.read())
+    return dialect.delimiter, 0 if has_header else None
