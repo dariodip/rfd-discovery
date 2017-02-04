335e5dbf697b4226071e9d021bb91000fefb095f
mattiatomeo <tomeo.mat@gmail.com>
2017-02-04 12:10:36 +0100

    Aggiustato il modulo plot_test.py ed aggiunto controllo sul separatore

--- tests/plot_tests.py
+++ tests/plot_tests.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+from utils.utils import check_sep_n_header
 plt.style.use('ggplot')
 
 
 def getfiles(dirpath):
     a = [s for s in os.listdir(dirpath)
          if os.path.isfile(os.path.join(dirpath, s)) and s.endswith(".csv")]
-    a.remove(".gitignore")
     a.sort(key=lambda s: os.path.getmtime(os.path.join(dirpath, s)))
     a.reverse()
     return a
@@ -17,7 +17,13 @@ def plot():
     dirpath = os.path.abspath("../resources/test")
     files = getfiles(dirpath)
     file_path = os.path.join(dirpath, files[0])
-    test_df = pd.read_csv(file_path, sep=";", decimal=',')
+    try:
+        sep, _ = check_sep_n_header(file_path)
+    except TypeError:
+        print("Unable to find separator in file ", files[0])
+        return
+
+    test_df = pd.read_csv(file_path, sep=sep, decimal='.')
     grouped_df = test_df.groupby(['ds_name']).mean()
     print(grouped_df)
     grouped_df['ds_file_size_B'].plot(y='time_elapsed')
