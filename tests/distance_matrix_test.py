a1883a4ca31273f985c96767ecc6740cf4b4d8de
mattiatomeo <tomeo.mat@gmail.com>
2017-02-05 20:36:01 +0100

    Aggiunto log in simple_start_test.py Ora si chiama distance_matrix_test.py

--- tests/distance_matrix_test.py
+++ tests/distance_matrix_test.py
@@ -1,15 +1,12 @@
 import unittest
-import os.path as op
-import pandas as pnd
-import numpy as np
 import utils.utils as ut
 import os
+import logging
 
-from math import sqrt
 from random import randint
 from loader import distance_mtr
-from ast import literal_eval
 
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 class MyTestCase(unittest.TestCase):
 
@@ -17,29 +14,33 @@ class MyTestCase(unittest.TestCase):
         path = "../resources"
         datasets = self.__load_all_files__(path)
         for ds in datasets:
+            logging.info("Next df: {}".format(ds))
             current_ds = path + "/" + ds
             try:
                 c_sep, has_header = ut.check_sep_n_header(current_ds)
             except Exception as ex:
                 print("Failed to load separator and header. Skipping test for {}".format(ds))
                 pass
+            logging.info("Loading distance matrix")
             dm = distance_mtr.DiffMatrix(current_ds, sep=c_sep, first_col_header=has_header)  # create class
             self.assertIsNotNone(dm, "check that dm is not null (none in python)")
             dist_m = dm.split_sides({'lhs': [1, 2, 3], 'rhs': [0]})  # split dm according to control RHS and LHS
-            split_rhs = dist_m['RHS']
+            split_rhs = dist_m.loc[:,'RHS']
             split_lhs = dist_m.drop('RHS', 1)
+            logging.info("Dataset loaded")
+            logging.info("Check splitting")
             self.assertIsNotNone(split_rhs, "check if split_rhs is not none")
             self.assertIsNotNone(split_lhs, "check if split_lhs is not none")
             self.assertGreater(len(split_rhs), 0, "check if split_rhs is not an empty list")
             self.assertGreater(len(split_lhs), 0, "check if split_lhs is not an empty list")
             self.assertEqual(len(split_rhs), len(split_lhs), "check if rhs and lhs length is the same")
-            #keys = dist_m.index
-            #expected_pairs = int(len(split_rhs) * (len(split_rhs) - 1) / 2)  # (n*n-1)/2 pairs on n els
-            #self.assertEqual(len(keys), expected_pairs, "check if number of pairs is n*(n-1)/2")
-            # generate a random pair of indexes
-            rnd1 = randint(1, len(split_rhs)- 1)
-            rand_row = dist_m.loc[rnd1]
+            logging.info("Check rows values")
+            rnd = randint(1, len(split_rhs)- 1)
+            rand_row = dist_m.loc[rnd]
             self.assertTrue(all(isinstance(item, float) for item in rand_row.tolist()), "check if each element is a float")
+            logging.info("Check the presence of NaN values")
+            self.assertFalse(dist_m.isnull().values.any(), "check if same value is NaN")
+            logging.info("All Ok!")
 
     @staticmethod
     def __load_all_files__(path="../resources") -> list:
