c10c5c83213d4f3ec352681825cd926932e0bcbb
mattiatomeo <tomeo.mat@gmail.com>
2017-02-05 19:54:05 +0100

    Inseriti i dataset da utilizzare ed aggiornato test simple_start_test testa correttamente la matrice delle distanze

--- tests/simple_start_test.py
+++ tests/simple_start_test.py
@@ -2,64 +2,48 @@ import unittest
 import os.path as op
 import pandas as pnd
 import numpy as np
+import utils.utils as ut
+import os
 
 from math import sqrt
 from random import randint
 from loader import distance_mtr
 from ast import literal_eval
 
-DATASET_CSV = "../resources/dataset.csv"
-DS_CSV = "../resources/distance-ds_test.csv"
-
 
 class MyTestCase(unittest.TestCase):
 
     def test_distance_mtr_control_example(self):
-        dm = distance_mtr.DiffMatrix(DATASET_CSV)  # create class
-        dm.load()  # load data set from csv
-        self.assertIsNotNone(dm.df, "check that dm is not null (none in python)")
-        split = dm.split_sides([1, 2, 3], [0])  # split dm according to control RHS and LHS
-        self.assertTrue('rhs' in split and 'lhs' in split, "check if 'rhs' and 'lhs' keys exist")
-        split_rhs = split['rhs']
-        split_lhs = split['lhs']
-        self.assertIsNotNone(split_rhs, "check if split_rhs is not none")
-        self.assertIsNotNone(split_lhs, "check if split_lhs is not none")
-        self.assertGreater(len(split_rhs), 0, "check if split_rhs is not an empty list")
-        self.assertGreater(len(split_lhs), 0, "check if split_lhs is not an empty list")
-        self.assertEqual(len(split_rhs), len(split_lhs), "check if rhs and lhs length is the same")
-        self.assertEqual((split_rhs.shape[1] + split_lhs.shape[1]), 4, "check if the sum of RHSs and LHSs is 4")
-        dist_m = dm.distance_matrix(split)  # create distance matrix according to split on RHS and LHS
-        keys = dist_m.index
-        expected_pairs = int(len(split_rhs) * (len(split_rhs) - 1) / 2)  # (n*n-1)/2 pairs on n els
-        self.assertEqual(len(keys), expected_pairs, "check if number of pairs is n*(n-1)/2")
-        # generate a random pair of indexes
-        rnd1 = randint(1, len(split_rhs)- 1)
-        rnd2 = randint(rnd1, len(split_rhs))
-        while rnd2 <= rnd1:
-            rnd2 = randint(1, len(split_rhs))
-        rand_index = (rnd1, rnd2)
-        rand_row = dist_m.loc[str(rand_index)]
-        self.assertTrue(all(isinstance(item, float) for item in rand_row.tolist()), "check if each element is an int")
-        h_distance_row = dist_m['RHS']
-        lhdr = h_distance_row.tolist()
-        self.assertTrue(all(lhdr[i] >= lhdr[i+1] for i in range(len(lhdr)-1)), "check if distance on RHS is sorted")
+        path = "../resources"
+        datasets = self.__load_all_files__(path)
+        for ds in datasets:
+            current_ds = path + "/" + ds
+            try:
+                c_sep, has_header = ut.check_sep_n_header(current_ds)
+            except Exception as ex:
+                print("Failed to load separator and header. Skipping test for {}".format(ds))
+                pass
+            dm = distance_mtr.DiffMatrix(current_ds, sep=c_sep, first_col_header=has_header)  # create class
+            self.assertIsNotNone(dm, "check that dm is not null (none in python)")
+            dist_m = dm.split_sides({'lhs': [1, 2, 3], 'rhs': [0]})  # split dm according to control RHS and LHS
+            split_rhs = dist_m['RHS']
+            split_lhs = dist_m.drop('RHS', 1)
+            self.assertIsNotNone(split_rhs, "check if split_rhs is not none")
+            self.assertIsNotNone(split_lhs, "check if split_lhs is not none")
+            self.assertGreater(len(split_rhs), 0, "check if split_rhs is not an empty list")
+            self.assertGreater(len(split_lhs), 0, "check if split_lhs is not an empty list")
+            self.assertEqual(len(split_rhs), len(split_lhs), "check if rhs and lhs length is the same")
+            #keys = dist_m.index
+            #expected_pairs = int(len(split_rhs) * (len(split_rhs) - 1) / 2)  # (n*n-1)/2 pairs on n els
+            #self.assertEqual(len(keys), expected_pairs, "check if number of pairs is n*(n-1)/2")
+            # generate a random pair of indexes
+            rnd1 = randint(1, len(split_rhs)- 1)
+            rand_row = dist_m.loc[rnd1]
+            self.assertTrue(all(isinstance(item, float) for item in rand_row.tolist()), "check if each element is a float")
 
-        #check if the distances correspond to the value in the oracle
-        oracle = pnd.read_csv("../resources/dataset_oracle.csv", sep=",", header=0, index_col=0)
-
-        for key in keys:
-            key_tuple = literal_eval(key) #extract the value from the tuple in a string form
-            row, col = key_tuple
-            a_tuple = dist_m.loc[key].tolist()
-            # complete the formula of the euclidean's distances of the R's method dist
-            distance = sqrt(sum(x**2 for x in a_tuple))
-            #oracle.loc[row index] return a Series with the index in string form
-            #never check the equality of two floating numbers with the equals' operator
-            self.assertTrue(np.isclose(distance, oracle.loc[row][str(col)]), "value error")
-
-        dist_m.to_csv(DS_CSV, sep=";")  # save distance csv in resources
-        self.assertTrue(op.isfile(DS_CSV), "check if distance matrix is saved")
-        self.assertTrue(op.exists(DS_CSV), "check if distance matrix is saved")
+    @staticmethod
+    def __load_all_files__(path="../resources") -> list:
+        return [file for file in os.listdir(path) if not file.startswith("distance") and file.endswith('.csv')]
 
 if __name__ == '__main__':
     unittest.main()
