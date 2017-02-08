c8edf63618723731ff9a0a6e906215c8fe28131e
mattiatomeo <tomeo.mat@gmail.com>
2017-02-08 17:15:10 +0100

    Terminata la documentazione Inserita la documentazione dei moduli di test

--- tests/distance_matrix_test.py
+++ tests/distance_matrix_test.py
@@ -7,11 +7,19 @@ import pandas as pnd
 from random import randint
 from loader import distance_mtr
 
+"""This module contain the testing code used for test the correctness of the class DiffMatrix"""
+
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 class MyTestCase(unittest.TestCase):
-
+    """
+    Class containing the code used for test the correctness of the class DiffMatrix
+    """
     def test_distance_mtr_control_example(self):
+        """
+        Check if the class DiffMatrix compute the distance matrix correctly. The code is tested with a series of
+        dataset contained in the variable path. For each step print a logging message.
+        """
         path = "../resources"
         datasets = self.__load_all_files__(path)
         for ds in datasets:
@@ -49,11 +57,36 @@ class MyTestCase(unittest.TestCase):
 
     @staticmethod
     def __load_all_files__(path="../resources") -> list:
+        """
+        Given a valid path of a directory, it return a list with all the CSV files' names contained in the directory
+        if that names do not start with the prefix 'distance'
+        :param path: valid path to a directory
+        :type path: str
+        :return: list of all the CSV files in path that do not start with the prefix 'distance'
+        :rtype: list
+        """
         return [file for file in os.listdir(path) if not file.startswith("distance") and file.endswith('.csv')]
 
     @staticmethod
     def __load_df(path, datetime=False, sep=';', missing='?', first_col_header=0, index_col=False):
-        """Load a pandas data frame from a csv file stored in the path df.
+        """
+        Load a pandas data frame from a CSV file stored in the path df.
+        :param path: valid path of a CSV file
+        :type path: str
+        :param datetime: denote if we want that some column in the dataset must be interpreted as a date.
+        To indicate that columns, whe use a list of integers indexes, where each index represent a column of the dataset
+        containing the dates. False value indicate that we do not have any column with date. Default behavior is as if set to False if no value is passed.
+        :type datetime: bool or list
+        :param sep: the separator used to separate the values in the CSV file. Default behavior is as if set to ';' if no value is passed.
+        :type sep: str
+        :param missing: the character used to notify a missing value. Default behavior is as if set to '?' if no value is passed.
+        :type missing: str
+        :param first_col_header: row number(s) to use as column names, and the start of the data. Default behavior is as if set to 0 if no value is passed.
+        :type first_col_header: int or list of int
+        :param index_col: the column which contains the primary key of the dataset. Specifying it, this will not calculate as distance.
+        :type index_col: int
+        :return: the data frame containing the CSV data
+        :rtype: pandas.core.frame.DataFrame
         """
         df = pnd.read_csv(path, sep=sep, header=first_col_header, index_col=index_col, engine='c',
                                na_values=['', missing], parse_dates=datetime)
--- tests/time_counting_test.py
+++ tests/time_counting_test.py
@@ -8,6 +8,9 @@ import utils.utils as ut
 from loader.distance_mtr import DiffMatrix
 from dominance.dominance_tools import RFDDiscovery
 
+"""
+This module is used to execute the algorithm defined in the class RFDDiscovery of the module dominance_tools.
+"""
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 cols = ['ds_name', 'ds_len', 'ds_attr_size', 'ds_file_size_B', 'time_elapsed', 'distance_time',
@@ -18,6 +21,20 @@ ITERATION_TIME = 1
 class MyTestCase(unittest.TestCase):
 
     def test_something(self):
+        """
+        This method execute the algorithm defined in the class RFDDiscovery for each dataset in the directory resources and for
+        each combination of rhs and lhs of them. For each execution of the algorithm, the method saves some information:
+            - the dataset's name;
+            - the dataset rows' number;
+            - number of column;
+            - the dataset file's size;
+            - the algorithm's elapsed time;
+            - the number of RFDs found;
+            - the combination of rhs and lhs used for the iteration;
+            - the number of the iteration executed on that combination.
+        When the test will end, it will save all the information described above in a CSV file with the name
+        <date of test>-result-c.csv. During the test, some log information will be printed.
+        """
         test_count = 1
         logging.info("Starting test")
         result_df = pd.DataFrame(columns=cols)  # Data frame in which save results
@@ -67,6 +84,17 @@ class MyTestCase(unittest.TestCase):
 
     @staticmethod
     def __get_diff_mtx(c_sep, current_ds, has_header):
+        """
+        Given a valid path to a dataset, load it and create the distance matrix with the class DiffMatrix
+        :param c_sep: separator used by the CSV file
+        :type c_sep: str
+        :param current_ds: valid path to a CSV file containing the dataset
+        :type current_ds: str
+        :param has_header: 0 if the CSV file has an header, None otherwise
+        :type has_header: bool
+        :return: the distance matrix of the dataset and the elapsed time used to build the matrix
+        :rtype: tuple
+        """
         start_time_dist = time.time()
         logging.info("Creating class DiffMatrix")
         logging.info("Class DiffMatrix created. Now loading file...")
@@ -80,6 +108,27 @@ class MyTestCase(unittest.TestCase):
     @staticmethod
     def __append_result(name: str, row_len: int, attr_size: int, file_size: int, elapsed_time: float,
                         dist_time_elaps: float, rdf_count: int, combination: str, df: pd.DataFrame) -> object:
+        """
+        Append to the data frame df, a row containing the values given as parameters.
+        :param name: a dataset's name
+        :type name: str
+        :param row_len: number of rows
+        :type row_len: int
+        :param attr_size: number of columns
+        :type attr_size: int
+        :param file_size: the dataset file's size
+        :type file_size: int
+        :param elapsed_time: time elapsed to find the RFDs
+        :type elapsed_time: float
+        :param dist_time_elaps: time elapsed to calculate the distance matrix
+        :type dist_time_elaps: float
+        :param rdf_count: number of RFDs found
+        :type rdf_count: int
+        :param combination: the combination of rhs and lhs used
+        :type combination: str
+        :param df: the data frame that will contain the row
+        :type df: pandas.core.frame.DataFrame
+        """
         now = datetime.now()
         id = now.strftime("%Y-%m-%d_%H:%M:%S.%f")
         row_to_add = [name, row_len, attr_size, file_size, elapsed_time, dist_time_elaps,
@@ -88,11 +137,31 @@ class MyTestCase(unittest.TestCase):
 
     @staticmethod
     def __load_all_files__(path="../resources") -> list:
+        """
+        Given a valid path of a directory, it return a list with all the CSV files' names contained in the directory
+        if that names do not start with the prefix 'distance'
+        :param path: valid path to a directory
+        :type path: str
+        :return: list of all the CSV files in path that do not start with the prefix 'distance'
+        :rtype: list
+        """
         logging.info("Loading all files")
         return [file for file in os.listdir(path) if not file.startswith("distance") and file.endswith('.csv')]
 
     @staticmethod
     def __get_ds_shape(ds: str, sep: str, first_row_head) -> dict:
+        """
+        Given a valid path of a dataset, return the number of rows and columns of the dataset.
+        :param ds: a valid path of a CSV file containing a dataset
+        :type ds: str
+        :param sep: separator used by the CSV file
+        :type c_sep: str
+        :param first_row_head: integer index of the first row of the dataset in the CSV file
+        :type first_row_head: int
+        :return: a dict containing on the key 'col' the number of columns, on the key 'row' the number of rows
+        and on the key 'col_caption' the header of the dataset.
+        :rtype: dict
+        """
         df = pd.read_csv(ds, sep=sep, header=first_row_head)
         shape = df.shape
         return {'col': shape[1], 'row': shape[0], 'col_caption': df.columns[1:]}
