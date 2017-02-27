b2472ddc9daa48510801e8eee8a611345e954792
mattiatomeo <mattiatomeo@github.com>
2017-02-27 11:06:25 +0100

    Corretto problema dei path

--- server/run.py
+++ server/run.py
@@ -1,4 +1,6 @@
+import os
 import sys
+sys.path.append(os.path.abspath(".."))
 import utils.utils as ut
 from loader.distance_mtr import DiffMatrix
 from dominance.dominance_tools import RFDDiscovery
